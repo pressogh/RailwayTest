e4add2efb3059e4281bb9b4701f9828978b5a9e5
PRESSO <caff1nepill@gmail.com>
2023-06-13 01:33:58 +0900

    update DEBUG

--- railway_test/settings.py
+++ railway_test/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     secret_file = os.path.join(BASE_DIR, 'secrets.json')
